f50406d37951dcffb2d7a315dff866808bc2a6b8
bahelms <jimbonkgit@gmail.com>
2014-11-20 06:39:13 -0800

    saves edits

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -2,7 +2,20 @@ Todos.TodoController = Ember.ObjectController.extend({
   actions: {
     editTodo: function() {
       this.set("isEditing", true);
-    }
+    },
+    acceptChanges: function() {
+      this.set("isEditing", false);
+      if (Ember.isEmpty(this.get("model.title"))) {
+       this.send("removeTodo");
+      } else {
+       this.get("model").save();
+      }
+    },
+    removeTodo: function() {
+      var todo = this.get("model");
+      todo.deleteRecord();
+      todo.save();
+    },
   },
 
   isEditing: false,
@@ -16,5 +29,5 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property("model.isCompleted")
+  }.property("model.isCompleted"),
 });
